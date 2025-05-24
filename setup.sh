ae90bd7c213c137b45f89f7ac0772c54de08546d
an-unsoken-vision <arpitsaxena1009@gmail.com>
2025-05-24 13:59:14 +0530

    Added setup.sh and updated requirements.txt for deployment

---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+# Install PyTorch CPU version from the official PyTorch wheel index
+pip install torch==2.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+
+# Then install other dependencies
+pip install -r requirements.txt
