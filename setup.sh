5b5c1dfaa1e871c7011002cd470b34ea7c5f8e01
an-unsoken-vision <arpitsaxena1009@gmail.com>
2025-05-24 14:04:03 +0530

    Fix torch install: remove from requirements, add setup.sh for CPU version

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 
 # Install PyTorch CPU version from the official PyTorch wheel index
-pip install torch==2.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==2.7.0+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+
 
 # Then install other dependencies
 pip install -r requirements.txt
